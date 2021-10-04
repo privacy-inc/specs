89f150e4a4ad62edaaf2173b4fd5b6df8ec5d29a
vaux <zero.griffin@gmail.com>
2021-10-04 15:49:03 +0200

    Add breaking tests

---
+++ Tests/PolicyBreakingTests.swift
@@ -0,0 +1,26 @@
+import XCTest
+@testable import Specs
+
+final class PolicyBreakingTests: XCTestCase {
+    private var policy: PolicyLevel!
+    private let list =  [
+        "https://consent.youtube.com/m?continue=https%3A%2F%2Fwww.youtube.com%2F&gl=DE&m=0&pc=yt&uxe=23983172&hl=en-GB&src=1",
+        "https://consent.google.com/?hl=en&origin=https://www.google.com&continue=https://www.google.com/search?q%3Dweather%2Bberlin&if=1&m=0&pc=s&wp=-1&gl=GR"
+    ]
+    
+    override func setUp() {
+        policy = Policy.Secure()
+    }
+    
+    func test() {
+        list
+            .map {
+                ($0, policy(URL(string: $0)!))
+            }
+            .forEach {
+                if case .allow = $0.1 { } else {
+                    XCTFail("\($0.1): \($0.0)")
+                }
+            }
+    }
+}

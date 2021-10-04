0be2134039d136a2f00df22391696619a598a6c6
vaux <zero.griffin@gmail.com>
2021-10-04 15:50:31 +0200

    Add external tests

---
+++ Tests/PolicyExternalTests.swift
@@ -0,0 +1,28 @@
+import XCTest
+@testable import Specs
+
+final class PolicyExternalTests: XCTestCase {
+    private var policy: PolicyLevel!
+    private let list = [
+        "some://www.ecosia.org",
+        "apps://www.theguardian.com/email/form/footer/today-uk",
+        "sms://uk.reuters.com/",
+        "dsddasdsa://"
+    ]
+
+    override func setUp() {
+        policy = Policy.Secure()
+    }
+    
+    func test() {
+        list
+            .map {
+                ($0, policy(URL(string: $0)!))
+            }
+            .forEach {
+                if case .external = $0.1 { } else {
+                    XCTFail("\($0.1): \($0.0)")
+                }
+            }
+    }
+}

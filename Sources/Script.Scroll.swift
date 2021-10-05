b76f6cf5875eb10532f07e31f9b423163422fc62
vaux <zero.griffin@gmail.com>
2021-10-05 14:09:53 +0200

    Update scripts

---
+++ Sources/Script.Scroll.swift
@@ -0,0 +1,56 @@
+import Foundation
+
+extension Script {
+    static let scroll = """
+const _privacy_incognit_splitted = location.hostname.split(".");
+if (_privacy_incognit_splitted.length > 1) {
+    switch (_privacy_incognit_splitted[_privacy_incognit_splitted.length - 2]) {
+    case "google":
+            var style = document.createElement('style');
+            style.innerHTML = ":root, body { overflow-y: visible !important; }";
+            document.head.appendChild(style);
+            break;
+    case "youtube":
+            var style = document.createElement('style');
+            style.innerHTML = "body { position: unset !important; }";
+            document.head.appendChild(style);
+            break;
+    case "instagram":
+            var style = document.createElement('style');
+            style.innerHTML = ":root, html, body, .E3X2T { overflow: unset !important; }";
+            document.head.appendChild(style);
+            break;
+    case "reuters":
+            var style = document.createElement('style');
+            style.innerHTML = ":root, html, body { overflow: unset !important; }";
+            document.head.appendChild(style);
+            break;
+    case "thelocal":
+            var style = document.createElement('style');
+            style.innerHTML = ".tp-modal-open { overflow: unset !important; }";
+            document.head.appendChild(style);
+            break;
+    case "pinterest":
+            document.body.setAttribute("style", "overflow-y: visible !important");
+            break;
+    case "bbc":
+            var style = document.createElement('style');
+            style.innerHTML = "body { overflow: unset !important; }";
+            document.head.appendChild(style);
+            break;
+    case "reddit":
+            var style = document.createElement('style');
+            style.innerHTML = "body, .scroll-disabled { overflow: unset !important; }";
+            document.head.appendChild(style);
+            break;
+    case "bloomberg":
+            var style = document.createElement('style');
+            style.innerHTML = "body { overflow: unset !important; }";
+            document.head.appendChild(style);
+            break;
+    default:
+            break;
+    }
+}
+"""
+}
--- Tests/SettingsConfigurationTests.swift
+++ Tests/SettingsConfigurationTests.swift
@@ -42,19 +42,23 @@ final class SettingsConfigurationTests: XCTestCase {
     
     func testTimers() {
         XCTAssertTrue(configuration.timers)
+        XCTAssertFalse(configuration.scripts.end.contains(Script.timers))
         
         configuration = configuration
             .with(timers: false)
+        XCTAssertTrue(configuration.scripts.end.contains(Script.timers))
         XCTAssertFalse(configuration.data.prototype(Settings.Configuration.self).timers)
     }
     
     func testDark() {
         XCTAssertTrue(configuration.dark)
         XCTAssertTrue(configuration._blockers.contains(.antidark))
+        XCTAssertTrue(configuration.scripts.start.contains(Script.dark))
         
         configuration = configuration
             .with(dark: false)
         XCTAssertFalse(configuration._blockers.contains(.antidark))
+        XCTAssertFalse(configuration.scripts.start.contains(Script.dark))
         XCTAssertFalse(configuration.data.prototype(Settings.Configuration.self).dark)
     }
     
@@ -71,10 +75,12 @@ final class SettingsConfigurationTests: XCTestCase {
     func testScreen() {
         XCTAssertFalse(configuration.screen)
         XCTAssertTrue(configuration._blockers.contains(.screen))
+        XCTAssertTrue(configuration.scripts.end.contains(Script.scroll))
         
         configuration = configuration
             .with(screen: true)
         XCTAssertFalse(configuration._blockers.contains(.screen))
+        XCTAssertFalse(configuration.scripts.end.contains(Script.scroll))
         XCTAssertTrue(configuration.data.prototype(Settings.Configuration.self).screen)
     }
     
@@ -115,170 +121,4 @@ final class SettingsConfigurationTests: XCTestCase {
                                     $0 != .third
                                 }), configuration._blockers)
     }
-    
-    
-    
-    
-    /*
-     func testInitial() {
-         XCTAssertTrue(Router.secure === settings.router)
-         XCTAssertEqual(.init(Blocker
-                                 .allCases
-                                 .filter {
-                                     $0 != .third
-                                 }), settings.blocking)
-     }
-     
-     func testEngine() {
-         settings.engine = .ecosia
-         XCTAssertEqual(.ecosia, (settings.pre + settings.post).prototype(Settings.self).engine)
-     }
-     
-     func testJavascript() {
-         settings.javascript = false
-         XCTAssertFalse((settings.pre + settings.post).prototype(Settings.self).javascript)
-     }
-     
-     func testDark() {
-         XCTAssertTrue((settings.pre + settings.post).prototype(Settings.self).blocking.contains(.antidark))
-         settings.dark = false
-         XCTAssertFalse(settings.blocking.contains(.antidark))
-         XCTAssertFalse((settings.pre + settings.post).prototype(Settings.self).dark)
-         XCTAssertFalse((settings.pre + settings.post).prototype(Settings.self).blocking.contains(.antidark))
-     }
-     
-     func testPopups() {
-         settings.popups = true
-         XCTAssertTrue((settings.pre + settings.post).prototype(Settings.self).popups)
-     }
-     
-     func testAds() {
-         XCTAssertTrue((settings.pre + settings.post).prototype(Settings.self).blocking.contains(.ads))
-         settings.ads = true
-         XCTAssertFalse(settings.blocking.contains(.ads))
-         XCTAssertFalse((settings.pre + settings.post).prototype(Settings.self).blocking.contains(.ads))
-         XCTAssertTrue((settings.pre + settings.post).prototype(Settings.self).ads)
-     }
-     
-     func testScreen() {
-         XCTAssertTrue((settings.pre + settings.post).prototype(Settings.self).blocking.contains(.screen))
-         settings.screen = true
-         XCTAssertFalse(settings.blocking.contains(.screen))
-         XCTAssertFalse((settings.pre + settings.post).prototype(Settings.self).blocking.contains(.screen))
-         XCTAssertTrue((settings.pre + settings.post).prototype(Settings.self).screen)
-     }
-     
-     func testTrackers() {
-         XCTAssertTrue(Router.secure === (settings.pre + settings.post).prototype(Settings.self).router)
-         settings.trackers = true
-         XCTAssertTrue(Router.regular === settings.router)
-         XCTAssertTrue((settings.pre + settings.post).prototype(Settings.self).trackers)
-         XCTAssertTrue(Router.regular === (settings.pre + settings.post).prototype(Settings.self).router)
-     }
-     
-     func testCookies() {
-         XCTAssertTrue((settings.pre + settings.post).prototype(Settings.self).blocking.contains(.cookies))
-         settings.cookies = true
-         XCTAssertFalse(settings.blocking.contains(.cookies))
-         XCTAssertFalse((settings.pre + settings.post).prototype(Settings.self).blocking.contains(.cookies))
-         XCTAssertTrue((settings.pre + settings.post).prototype(Settings.self).cookies)
-     }
-     
-     func testHttp() {
-         XCTAssertTrue((settings.pre + settings.post).prototype(Settings.self).blocking.contains(.http))
-         settings.http = true
-         XCTAssertFalse(settings.blocking.contains(.http))
-         XCTAssertFalse((settings.pre + settings.post).prototype(Settings.self).blocking.contains(.http))
-         XCTAssertTrue((settings.pre + settings.post).prototype(Settings.self).http)
-     }
-     
-     func testLocation() {
-         settings.location = true
-         XCTAssertTrue((settings.pre + settings.post).prototype(Settings.self).location)
-     }
-     
-     func testRules() {
-         settings.dark = false
-         settings.ads = true
-         settings.cookies = true
-         settings.http = true
-         settings.screen = true
-         settings.third = true
-         XCTAssertTrue(settings.blocking.isEmpty)
-         
-         settings.ads = false
-         XCTAssertEqual([.ads], settings.blocking)
-         
-         settings.ads = true
-         settings.cookies = false
-         XCTAssertEqual([.cookies], settings.blocking)
-         
-         settings.cookies = true
-         settings.http = false
-         XCTAssertEqual([.http], settings.blocking)
-         
-         settings.http = true
-         settings.screen = false
-         XCTAssertEqual([.screen], settings.blocking)
-         
-         settings.screen = true
-         settings.third = false
-         XCTAssertEqual([.third], settings.blocking)
-     }
-     
-     func testThird() {
-         settings.engine = .ecosia
-         XCTAssertTrue((settings.pre + settings.post).prototype(Settings.self).third)
-         XCTAssertEqual(.ecosia, (settings.pre + settings.post).prototype(Settings.self).engine)
-     }
-     
-     func testThirdCombined() {
-         var archive = Archive.new
-         archive.settings.engine = .ecosia
-         archive.settings.third = false
-         XCTAssertFalse(archive.data.prototype(Archive.self).settings.third)
-         XCTAssertEqual(.ecosia, archive.data.prototype(Archive.self).settings.engine)
-     }
-     
-     func testTimers() {
-         settings.engine = .ecosia
-         XCTAssertTrue((settings.pre + settings.post).prototype(Settings.self).timers)
-         XCTAssertEqual(.ecosia, (settings.pre + settings.post).prototype(Settings.self).engine)
-     }
-     
-     func testTimersCombined() {
-         var archive = Archive.new
-         archive.settings.engine = .ecosia
-         archive.settings.timers = false
-         XCTAssertFalse(archive.data.prototype(Archive.self).settings.timers)
-         XCTAssertEqual(.ecosia, archive.data.prototype(Archive.self).settings.engine)
-     }
-     
-     
-     
-     
-     
-     
-     
-     
-     func testBegin() {
-         XCTAssertEqual(Script.dark + Script.favicon, settings.start)
-         settings.dark = false
-         XCTAssertEqual(Script.favicon, settings.start)
-     }
-     
-     func testEnd() {
-         XCTAssertEqual(Script.scroll, settings.end)
-         settings.location = true
-         XCTAssertEqual(Script.scroll + Script.location, settings.end)
-         settings.timers = false
-         XCTAssertEqual(Script.scroll + Script.location + Script.timers, settings.end)
-         settings.location = false
-         XCTAssertEqual(Script.scroll + Script.timers, settings.end)
-         settings.location = false
-         settings.timers = true
-         settings.screen = true
-         XCTAssertTrue(settings.end.isEmpty)
-     }
-     */
 }

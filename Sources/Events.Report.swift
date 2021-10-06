ecd0b79a1cfbb2e280bc5cd299da0042096e74eb
vaux <zero.griffin@gmail.com>
2021-10-06 18:09:01 +0200

    Make report identifiable

--- Sources/Events.Report.swift
+++ Sources/Events.Report.swift
@@ -1,9 +1,17 @@
 import Foundation
 
 extension Events {
-    public struct Report {
+    public struct Report: Identifiable {
+        public let id: String
         public let date: Date
         public let website: String
         public let trackers: [String]
+        
+        init(date: Date, website: String, trackers: [String]) {
+            self.date = date
+            self.website = website
+            self.trackers = trackers
+            id = date.description + website
+        }
     }
 }

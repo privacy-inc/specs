782b3c652a6584a9c94ce745fe2ee3ffd64c7764
vaux <zero.griffin@gmail.com>
2021-11-13 17:45:35 +0100

    Handle promise error

--- Sources/Script.Location.swift
+++ Sources/Script.Location.swift
@@ -3,17 +3,21 @@ import Foundation
 extension Script {
     static let _location = """
 navigator.geolocation.getCurrentPosition = async function(success, error, options) {
-    var promise = await window.webkit.messageHandlers.\(Script.location.method).postMessage("\(Script.location.method)");
+    try {
+        var promise = await window.webkit.messageHandlers.\(Script.location.method).postMessage("\(Script.location.method)");
 
-    if (success != null, promise != null) {
-        var position = {
-           coords: {
-               latitude: promise[0],
-               longitude: promise[1],
-               accuracy: promise[2]
-           }
-        };
-        success(position);
+        if (success != null, promise != null) {
+            var position = {
+               coords: {
+                   latitude: promise[0],
+                   longitude: promise[1],
+                   accuracy: promise[2]
+               }
+            };
+            success(position);
+        }
+    } catch(e) {
+        error(e);
     }
 };
 

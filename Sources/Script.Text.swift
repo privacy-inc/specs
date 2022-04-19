b5d0c0a6cc9aa68b43cd75cf76c076ec8ffb0759
vaux <zero.griffin@gmail.com>
2022-04-19 12:29:06 +0200

    Make double instead of cgfloat

--- Sources/Script.Text.swift
+++ Sources/Script.Text.swift
@@ -1,10 +1,8 @@
-import CoreGraphics
-
 extension Script {
     static let _text = """
 document.body.style.webkitTextSizeAdjust
 """
-    public static func text(size: CGFloat) -> String {
+    public static func text(size: Double) -> String {
         _text + "='\(Int(size * 100))%'"
     }
 }

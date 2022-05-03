e2d039958c0daff2e998b7887085c10bfc39c9e9
vaux <zero.griffin@gmail.com>
2022-05-03 12:44:46 +0200

    Update scripts

--- Sources/Script.Basic.swift
+++ Sources/Script.Basic.swift
@@ -5,22 +5,24 @@ extension Script {
 localStorage.clear();
 sessionStorage.clear();
 
-function privacy_finder(query) {
+function _privacy_incognit_finder(query) {
     var result = null;
     var item = window.getSelection().anchorNode;
     
-    while (item != null && item.tagName != "BODY" && item.tagName != "HTML" && item.querySelectorAll != null) {
-        const elements = item.querySelectorAll(query);
-
-        for (var i = 0; i < elements.length; i++) {
-            const source = elements[i].getAttribute("src");
+    while (item != null && item.tagName != "BODY" && item.tagName != "HTML") {
+        if (item.querySelectorAll != null) {
+            const elements = item.querySelectorAll(query);
             
-            if (source != null) {
-                result = source;
-                break;
+            for (var i = 0; i < elements.length; i++) {
+                const source = elements[i].src;
+                
+                if (source != null) {
+                    result = source;
+                    break;
+                }
             }
         }
-        
+    
         if (result == null) {
             item = item.parentNode;
         } else {
